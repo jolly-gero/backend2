0552d8d95c2afc234c14832af6cf39b68efb3db8
JOLLY@SDWAN <jolly260840@gmail.com>
2020-10-21 16:41:39 +0700

    remove not use

--- app/config/db.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  HOST: "203.150.221.207",
-  PORT: 27017,
-  DB: "bas",
-  USER: "root",
-  PWD: "vKwrDP8fMnnon0scszzEqFaPxocBC1y/b8rOUVzJ8k8=",
-  PWD_EN: "vKwrDP8fMnnon0scszzEqFaPxocBC1y%2Fb8rOUVzJ8k8%3D"
-};
