f4e6f9015ef9d224f7de20a9f4c8ece9f66de7c0
JOLLY@SDWAN <jolly260840@gmail.com>
2020-10-22 15:19:12 +0700

    test jolly

---
+++ example/test3.js
@@ -0,0 +1,18 @@
+const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
+
+// //Hash password
+// const salt = await bcrypt.genSalt(10);
+// const hashPassword = await bcrypt.hash(req.body.password, salt)
+// console.log(hashPassword);
+
+ // Password is correct ?
+ const validPass = await bcrypt.compare(req.body.password, user.password)
+ if(!validPass) return res.status(400).send('Invalid password !!')
+
+let A = bcrypt.hashSync("123456789",bcrypt.genSaltSync(10))
+console.log(A);
+console.log(bcrypt.compare("123456789",A));
+console.log("pull from data base");
+console.log(bcrypt.compare("123456789","$10$KzYgUDxQwQ9Qwj3fqsjJ0uXwOQw//ULJ5Dlj45DtGDhiZd/DgGGxm"));
+
